a9d56429742c9042b30c52d3bb3c96de9fbb4739
Trương Thanh Tâm <tamtruong037@gmail.com>
2021-06-07 10:41:21 +0700

    Improve cat eat method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 var Cat = require('./cat');
+var Mouse = require('./mouse');
+var Dog = require('./dog');
+
+var cat = new Cat();
+var mouse = new Mouse('Mickey');
+var dog = new Dog();
+try {
+	cat.eat(dog);
+} catch (error) {
+	console.log('Error while cat eating a dog');
+}
+console.log(cat);
