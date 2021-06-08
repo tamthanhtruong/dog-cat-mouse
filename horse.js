901938b5874f81c5ff8a6807c3336371aea7a5a4
Trương Thanh Tâm <tamtruong037@gmail.com>
2021-06-08 11:16:53 +0700

    Horse jump method

--- horse.js
+++ horse.js
@@ -2,11 +2,16 @@ function Horse() {
 }
 
 Horse.prototype.jump = function() {
+<<<<<<< HEAD
 	console.log('Jumped...');
 };
 
 Horse.prototype.run = function() {
 	console.log('Running');
 };
+=======
+	console.log('Jumped..');
+}
+>>>>>>> b1ecbfa (Horse jump method)
 
 module.exports = Horse;
