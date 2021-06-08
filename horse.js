0f5e8ef0ea11f3b7d63bc4e396893a69211f89ae
Trương Thanh Tâm <tamtruong037@gmail.com>
2021-06-08 22:53:05 +0700

    Updated again horse run method

--- horse.js
+++ horse.js
@@ -1,22 +1,12 @@
 function Horse() {
 }
 
-<<<<<<< HEAD
 Horse.prototype.jump = function() {
-<<<<<<< HEAD
 	console.log('Jumped...');
 };
 
 Horse.prototype.run = function() {
 	console.log('Running');
 };
-=======
-	console.log('Jumped..');
-=======
-Horse.prototype.run = function() {
-	console.log('Running..');
->>>>>>> b81afda (Horse run method update)
-}
->>>>>>> b1ecbfa (Horse jump method)
 
 module.exports = Horse;
